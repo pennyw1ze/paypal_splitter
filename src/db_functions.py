c1b5a148cd7a4a2ed27f8f98dc0534476e9a5761
darknessfriend <leo.rufini.459@gmail.com>
2025-01-27 22:46:00 +0100

    Implemented functions to explore the database, new database tables, function to get own money, function to increase own money (only for admin)

--- src/db_functions.py
+++ src/db_functions.py
@@ -13,6 +13,15 @@ def build_database():
                 spotify_amount REAL DEFAULT 0,
                 ripetizioni_amount REAL DEFAULT 0
               )''')
+    
+    c.execute('''CREATE TABLE IF NOT EXISTS translate(
+                id INTEGER PRIMARY KEY,
+                name TEXT
+              )''')
+    c.execute('''CREATE TABLE IF NOT EXISTS username(
+                telegram_id INTEGER PRIMARY KEY,
+                username TEXT
+              )''')
 
     # Commit changes
     conn.commit()
@@ -20,8 +29,23 @@ def build_database():
     # Close connection
     conn.close()
 
+# Check if a user is in the database
+def check_user_table(telegram_id):
+    # Connect to database
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    # Check if user is in the database
+    c.execute("SELECT * FROM users WHERE telegram_id=?", (telegram_id,))
+    user = c.fetchone()
+
+    # Close connection
+    conn.close()
+
+    return user
+
 # Add a user to the database
-def add_user(telegram_id):
+def add_user_table(telegram_id):
     # Connect to database
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
@@ -80,3 +104,117 @@ def get_all_users():
 
     return users
 
+# Get all names
+def get_all_translate():
+    # Connect to database
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    # Get all users
+    c.execute("SELECT * FROM translate")
+    translate = c.fetchall()
+
+    # Close connection
+    conn.close()
+
+    return translate
+
+# Add a name to the database
+def add_translate(id, name):
+    # Connect to database
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    # Add user ( or replace if already exists )
+    c.execute("INSERT OR REPLACE INTO translate VALUES (?, ?)", (id, name))
+
+    # Commit changes
+    conn.commit()
+
+    # Close connection
+    conn.close()
+
+
+# Add a username to the database
+def add_username(id, username):
+    # Connect to database
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    # Add user ( or replace if already exists )
+    c.execute("INSERT OR REPLACE INTO username VALUES (?, ?)", (id, username))
+
+    # Commit changes
+    conn.commit()
+
+    # Close connection
+    conn.close()
+
+# Check if a name is in the database
+def check_translate_name(name):
+    # Connect to database
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    # Check if user is in the database
+    c.execute("SELECT * FROM translate WHERE name=?", (name,))
+    user = c.fetchone()
+
+    # Close connection
+    conn.close()
+    
+    return user
+
+# Add netflix amount
+def add_netflix_amount(telegram_id, amount):
+    # Connect to database
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    # Get user
+    user = get_user(telegram_id)
+
+    # Update user
+    c.execute("UPDATE users SET netflix_amount=? WHERE telegram_id=?", (amount, telegram_id))
+
+    # Commit changes
+    conn.commit()
+
+    # Close connection
+    conn.close()
+
+# Add spotify amount
+def add_spotify_amount(telegram_id, amount):
+    # Connect to database
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    # Get user
+    user = get_user(telegram_id)
+
+    # Update user
+    c.execute("UPDATE users SET spotify_amount=? WHERE telegram_id=?", (amount, telegram_id))
+
+    # Commit changes
+    conn.commit()
+
+    # Close connection
+    conn.close()
+
+# Add ripetizioni amount
+def add_ripetizioni_amount(telegram_id, amount):
+    # Connect to database
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+
+    # Get user
+    user = get_user(telegram_id)
+
+    # Update user
+    c.execute("UPDATE users SET ripetizioni_amount=? WHERE telegram_id=?", (amount, telegram_id))
+
+    # Commit changes
+    conn.commit()
+
+    # Close connection
+    conn.close()
--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 ###################################################################
 # IMPORT LIBRARIES
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Application, CallbackQueryHandler
 # Import database functions
 from db_functions import *
+###################################################################
+# Mount the database
+build_database()
 ###################################################################
 # GLOBAL VARIABLES
 
@@ -15,15 +18,8 @@ Spotify = ["@danidarge","@skhuuuu"]
 # Admin ID
 Admin = 351523902
 
-# Users to ID dicrtionary
-translate = {
-    351523902: "@Leon4rd002",
-    0: "@brunette105",
-    1: "@danidarge",
-    2: "@skhuuuu",
-    3: "@grev8"
-}
-
+# Translate dictionary
+translate = {key: value for key, value in get_all_translate()}
 ###################################################################
 # GENERAL PURPOSE FUNCTIONS
 
@@ -35,6 +31,10 @@ def get_token():
     
 # Get pretty formatted lists from database outcomes
 def format(data):
+    # Check if the list is empty
+    if len(data) == 0:
+        return "Non ci sono utenti registrati."
+    # Format the list
     formatted = ""
     for user in data:
         formatted += f"{translate[user[0]]}:\n- Netflix: {user[1]}€;\n- Spotify: {user[2]}€;\n- Ripetizioni: {user[3]}€\n-----------------------\n"
@@ -51,24 +51,110 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Lorem ipsum dolor sit amet.\nIn caso di problemi contattare @Leon4rd002')
 
+# Define add user command handler
+async def add_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Save user id
+    id = update.effective_user.id
+    # Check if the user is already registered
+    if check_user_table(id) is not None:
+        await update.message.reply_text(f"Sembra che tu sia già registrato!")
+        return None
+    # Add user to database
+    add_user_table(id)
+    add_translate(id, update.effective_user.first_name)
+    if update.effective_user.username is not None:
+        add_username(id, update.effective_user.username)
+    # Update translate dictionary
+    global translate
+    translate = {key: value for key, value in get_all_translate()}
+    await update.message.reply_text(f"Sei stato aggiunto con successo!")
+
 # Define pagamenti command handler
 async def pagamenti(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Check if the user is in the database
+    if check_user_table(update.effective_user.id) is None:
+        await update.message.reply_text("Non sei registrato! Usa il comando /aggiungimi per registrarti.")
+        return None
     # Check if the user is the admin
     if update.effective_user.id == Admin:
         await update.message.reply_text(format(get_all_users()))
-    else:
-        id = update.effective_user.id
-        user = get_user(id)
-        message = "Ciao {translate[id]}!\n"
-        if user[1] + user[2] + user[3] == 0:
-            await update.message.reply_text(message + "Non devi pagare niente, sei a posto!\n")
-        else:
-            if 
+        return None
+    # Get user data
+    id = update.effective_user.id
+    user = get_user(id)
+    message = f"Ciao {translate[id]}!\n"
+    n,s,r = user[1],user[2],user[3]
+    # Check if the user has to pay
+    if user[1] + user[2] + user[3] == 0:
+        await update.message.reply_text(message + "Non devi pagare niente, sei a posto!\n")
+        return None
+    # Create message
+    message += "Ti rimangono da pagare:\n"
+    if user[1] > 0:
+        message += f"> {n}€ per Netflix.\n"
+    if user[2] > 0:
+        message += f"> {s}€ per Spotify.\n"
+    if user[3] > 0:
+        message += f"> {r}€ per le ripetizioni.\n"
+    # Sends a message with three inline buttons attached
+    keyboard = [
+        [button for button in [
+            InlineKeyboardButton(f"Salda Netflix ({n}€)", callback_data="1") if n > 0 else None,
+            InlineKeyboardButton(f"Salda Spotify ({s}€)", callback_data="2") if s > 0 else None,
+            InlineKeyboardButton(f"Salda Ripetizioni ({r}€)", callback_data="3") if r > 0 else None,
+        ] if button is not None]
+    ]
+
+    if n*s > 0:
+        keyboard.append([InlineKeyboardButton(f"Salda tutto ({n+s+r})€", callback_data="3")])
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(message, reply_markup=reply_markup)
+
+# Define add amount command handler
+async def add_amount(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Get user id
+    id = update.effective_user.id
+    # Check if the user is the admin
+    if id != Admin:
+        await update.message.reply_text("Non sei autorizzato ad eseguire questa operazione.")
+        return None
+    # Check if the command is formatted correctly
+    if len(context.args) != 3:
+        await update.message.reply_text("Il comando non è stato formattato correttamente.")
+        return None
+    # Message formatting: /aumenta <name> <service> <amount>
+    name = context.args[0]
+    service = context.args[1]
+    amount = float(context.args[2])
+    # Check if the user is in the database
+    user = check_translate_name(name)
+    if user is None:
+        await update.message.reply_text("L'utente non è registrato.")
+        return None
+    # Check if the service is valid
+    if service not in ["Netflix", "Spotify", "Ripetizioni"]:
+        await update.message.reply_text("Il servizio non è valido.")
+        return None
+    # Get payment data from user
+    user = get_user(id)
+    print(user)
+    # Update the database
+    if service == "Netflix":
+        add_netflix_amount(user[0], amount + float(user[1]))
+    elif service == "Spotify":
+        add_spotify_amount(user[0], amount + float(user[2]))
+    elif service == "Ripetizioni":
+        add_ripetizioni_amount(user[0], amount + float(user[3]))
+    # Send confirmation message
+    await update.message.reply_text(f"Aggiornamento completato.")
+
+
 ###################################################################
 # MAIN FUNCTION
 
 # Define main function
-def main(cursor) -> None:
+def main() -> None:
     # Debug message
     print("Bot started")
     
@@ -76,13 +162,20 @@ def main(cursor) -> None:
     app = ApplicationBuilder().token(get_token()).build()
 
     # Handle start command
-    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("inizia", start))
 
     # Handle help command
     app.add_handler(CommandHandler("aiuto", help))
 
     # Handle pagamenti command
     app.add_handler(CommandHandler("pagamenti", pagamenti))
+
+    # Handle add me" command
+    app.add_handler(CommandHandler("aggiungimi", add_user))
+
+    # ADMIN COMMANDS
+    # Handle Increasing and decreasing amounts
+    app.add_handler(CommandHandler("aumenta", add_amount))
 
     # Run polling
     app.run_polling()
@@ -91,12 +184,9 @@ def main(cursor) -> None:
 # MAIN FUNCTION
 
 if __name__ == '__main__':
-    # Function to build a local database that returns a cursor to navigate it
-    cursor = build_database()
-    # Add the admin
-    add_user(Admin)
+    print(translate)
     # Main function
-    main(cursor)
+    main()
 
 ###################################################################
 # END OF FILE
